c66c8d93b1f2fb20eea7e683070b7b15548bbffa
spirin-vadim <vadis99@ya.ru>
2023-07-15 20:09:31 +0500

    Сделал страницу регистрации нового участника

--- myapi/models.py
+++ myapi/models.py
@@ -1,15 +1,17 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.contrib.auth.models import AbstractUser
 
 class MyUser(AbstractUser):
     GENDERS = [
         ('М', 'Мужчина'),
         ('Ж', 'Женщина'),
     ]
-    photo = models.ImageField(upload_to='media/photo', blank=True, null=True)
+
     gender = models.CharField(max_length=7, choices=GENDERS)
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     email = models.EmailField(unique=True)
+    photo = models.ImageField(upload_to='media/photo', blank=True, null=True)
     def __str__(self):
+        self.username = self.username + ' ' + self.first_name + ' ' + self.last_name
         return self.username
--- myapi/views.py
+++ myapi/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+from .forms import MyUserCreationForm
+from .models import *
+
+menu = [
+    {'title' : 'Главная страница', 'url_name' : 'main'},
+]
+
+def main(request):
+    return render(request,
+                  'myapi/main.html',
+                  {'title' : 'Главная страница', 'menu' : menu})
+
+
+
+def create(request):
+    if request.method == 'POST':
+        form = MyUserCreationForm(request.POST, request.FILES)
+        if form.is_valid():
+            user = form.save()
+            user.save()
+            return redirect('main')
+        context = {'form': form}
+        return render(request, 'myapi/create.html', context)
+    context = {'form': MyUserCreationForm()}
+    return render(request, 'myapi/create.html', context)
